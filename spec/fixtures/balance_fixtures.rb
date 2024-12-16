4a8fedab0c926b264469f6d483e795e577e2a1a2
Sergey Karpuk <sergeykarpuk@yandex.by>
2024-12-16 16:33:03 +0300

    BOAPI-468 test(app): add psp_balance spec

--- spec/fixtures/balance_fixtures.rb
+++ spec/fixtures/balance_fixtures.rb
@@ -4,6 +4,49 @@
 module BalancesFixtures
   module_function
 
+  def successful_balances_for_psp_response_message
+    {
+      'balances' => {
+        'generated_at' => '2024-08-30T13:02:00Z',
+        'as_of_date' => '2024-09-13T00:00:00.145823Z',
+        'currencies' => [
+          {
+            'currency' => 'BYN',
+            'merchants' => [
+              {
+                'id' => 47,
+                'company_name' => 'John Deere LTD',
+                'available_balance' => 300,
+                'shops' => [
+                  {
+                    'id' => 1,
+                    'name' => 'Demo Shop 1',
+                    'available_balance' => 200,
+                    'gateways' => [
+                      { 'id' => 2, 'type' => 'AlternativeBank', 'available_balance' => 100 },
+                      { 'id' => 1, 'type' => 'MTVBank', 'available_balance' => 100 }
+                    ]
+                  },
+                  {
+                    'id' => 2,
+                    'name' => 'Demo Shop 2',
+                    'available_balance' => 100,
+                    'gateways' => [
+                      { 'id' => 2, 'type' => 'AlternativeBank', 'available_balance' => 100 }
+                    ]
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      }
+    }
+  end
+
+  def successful_balances_for_psp_response
+    %({"data":#{successful_balances_for_psp_response_message.to_json}})
+  end
   def successful_merchant_balances_for_psp_response_message
     {
       'balances' => {
